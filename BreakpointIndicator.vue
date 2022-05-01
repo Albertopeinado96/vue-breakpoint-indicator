7308afc77e0972c8ba49000c8e6a6fac8a2b31ab
Alberto Peinado <albertopeisan@gmail.com>
2022-05-01 12:27:36 +0200

    first commit

---
+++ BreakpointIndicator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div
+      class="xl:bg-blue-500 fixed top-0 right-0 m-4 mt-20 ml-12 flex h-8 w-8 items-center justify-center rounded-full bg-gray-700 p-3 font-mono text-xl text-white sm:bg-pink-500 md:bg-yellow-500 lg:bg-green-500"
+      style="z-index: 999"
+    >
+      <div class="xl:hidden block sm:hidden md:hidden lg:hidden">al</div>
+      <div class="xl:hidden hidden sm:block md:hidden lg:hidden">sm</div>
+      <div class="xl:hidden hidden sm:hidden md:block lg:hidden">md</div>
+      <div class="xl:hidden hidden sm:hidden md:hidden lg:block">lg</div>
+      <div class="xl:block hidden sm:hidden md:hidden lg:hidden">xl</div>
+    </div>
+  </div>
+</template>
